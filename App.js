4af102c188f045e2eaef059fd3850929cff20e06
Angel Domínguez Plata <angeldomp49@gmail.com>
2021-03-31 16:32:35 -0600

    react-navigation

--- App.js
+++ App.js
@@ -8,48 +8,10 @@ import "mdbreact/dist/css/mdb.css";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to react native</Text>
-      <Fragment>
-      
-      </Fragment>
-      
-      <StatusBar style="auto" />
-      <MDBFooter color="blue" className="font-small pt-4 mt-4">
-      <MDBContainer fluid className="text-center text-md-left">
-        <MDBRow>
-          <MDBCol md="6">
-            <h5 className="title">Footer Content</h5>
-            <p>
-              Here you can use rows and columns here to organize your footer
-              content.
-            </p>
-          </MDBCol>
-          <MDBCol md="6">
-            <h5 className="title">Links</h5>
-            <ul>
-              <li className="list-unstyled">
-                <a href="#!">Link 1</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Link 2</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Link 3</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Link 4</a>
-              </li>
-            </ul>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
-      <div className="footer-copyright text-center py-3">
-        <MDBContainer fluid>
-          &copy; {new Date().getFullYear()} Copyright: <a href="https://www.mdbootstrap.com"> MDBootstrap.com </a>
-        </MDBContainer>
-      </div>
-    </MDBFooter>
+    <View style={{ flex: 1,justifyContent: 'flex-end', alignItems: 'center', backgroundColor: 'yellow' }}>
+      <View style={{ height: 50, width: 50, backgroundColor: 'red' }} ></View>
+      <View style={{ height: 50, width: 50, backgroundColor: 'green' }} ></View>
+      <View style={{ height: 50, width: 50, backgroundColor: 'blue' }} ></View>
     </View>
   );
 }
